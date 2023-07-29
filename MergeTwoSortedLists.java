b29d502718debf4bc4b3886d929f084b41311b04
victorhorton <vichorton5@gmail.com>
2023-07-29 15:10:56 -0500

    Switch Merge Two Lists solution to use recursion

--- MergeTwoSortedLists.java
+++ MergeTwoSortedLists.java
@@ -10,49 +10,68 @@ class MergeTwoSortedLists {
         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
     }
 
-    public static ListNode mergeTwoSortedLists(ListNode list1, ListNode list2) {
-        ListNode merged_list = new ListNode();
-        ListNode beginning_of_merged_list = merged_list;
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-        // Runs until list1 or list2 runs out of values
         while (list1 != null && list2 != null) {
+
             if (list1.val <= list2.val) {
-                // Adds list1 value to merged list and moves to next ListNode value
-                merged_list.next = new ListNode(list1.val);
-                list1 = list1.next;
-            } else if (list1.val > list2.val) {
-                // Adds list2 value to merged list and moves to next ListNode value
-                merged_list.next = new ListNode(list2.val);
-                list2 = list2.next;
+
+                // Sets the remaining of list1 to be the greater of the two arguments
+                list1.next = mergeTwoLists(list1.next, list2);
+
+                // Returns list1 with the new remainging values
+                return list1;
+            }  else {
+
+                // Sets the remaining of list2 to be the greater of the two arguments
+                list2.next = mergeTwoLists(list1, list2.next);
+
+                // Returns list2 with the new remainging values
+                return list2;
             }
-
-            merged_list = merged_list.next;
         }
 
-        // Adds remaining list1 values if longer than list2
-        while (list1 != null) {
-            merged_list.next = new ListNode(list1.val);
-            list1 = list1.next;
-            merged_list = merged_list.next;
+        // Returns the remaining values once either list is empty
+        if (list2 == null) {
+            return list1;
+        } else {
+            return list2;
         }
 
-        // Adds remaining list2 values if longer than list1
-        while (list2 != null) {
-            merged_list.next = new ListNode(list2.val);
-            list2 = list2.next;
-            merged_list = merged_list.next;
-        }
-
-        // Returns the merge list starting at the beginning node
-        return beginning_of_merged_list.next;
     }
 
   public static void main(String[] args) {
 
     ListNode first_list_test_1 = new ListNode(1, new ListNode(2, new ListNode(4)));
-    ListNode second_list_test_1 = new ListNode(1, new ListNode(3, new ListNode(4)));
+    ListNode second_list_test_1 = new ListNode(1, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(10)))));
 
-    ListNode results = mergeTwoSortedLists(first_list_test_1, second_list_test_1);
+    ListNode results = mergeTwoLists(first_list_test_1, second_list_test_1);
+
+    System.out.print("112344510 = ");
+    while (results != null) {
+        System.out.print(results.val);
+        results = results.next;
+    }
+
+/*
+    Runtime order
+
+    |-results = mergeTwoLists([1,2,4], [1,3,4,5,10]);
+    | |-list1.next = mergeTwoLists([2,4], [1,3,4,5,10]);
+    | | |-list2.next = mergeTwoLists([2,4], [3,4,5,10]);
+    | | | |-list1.next = mergeTwoLists([4], [3,4,5,10]);
+    | | | | |-list2.next = mergeTwoLists([4], [4,5,10]);
+    | | | | | |-list1.next = mergeTwoLists([null], [4,5,10]);
+    | | | | | |
+    | | | | | |
+    | | | | | |-return list2 = [4, 5, 10]
+    | | | | |-return list1 = [4, 4, 5, 10]
+    | | | |-return list2 = [3, 4, 4, 5, 10]
+    | | |-return list2 = [2, 3, 4, 4, 5, 10]
+    | |-return list2 = [1, 2, 3, 4, 4, 5, 10]
+    |-return list2 = [1, 1, 2, 3, 4, 4, 5, 10]
+
+*/
 
  }
 }

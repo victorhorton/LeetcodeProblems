8fda1be5618853e1fca32acf8719c3925249d3e4
victorhorton <vichorton5@gmail.com>
2023-07-30 14:56:37 -0500

    Solve Climbing Stairs leetcode problem

---
+++ ClimbingStairs.java
@@ -0,0 +1,44 @@
+public class ClimbingStairs {
+
+  public static int climbStairs(int n) {
+    // Sets up Fibnacci's sequence
+    int leading_value = 1;
+    int trailing_value = 1;
+
+    // Calculates sequence n times
+    for (int i = 0; i < n; i++) {
+      int temp_one = leading_value;
+      leading_value += trailing_value;
+      trailing_value = temp_one;
+    }
+
+    // Returns the value of the nth calculation
+    return trailing_value;
+
+    /*
+      Fibnacci's sequence
+      1 1 2 3 5 8 13 21 34...
+      First Calculation   1 + 1   = 2
+      Second Calculation  1 + 2   = 3
+      Third Calculation   2 + 3   = 5
+      Fourth Calculation  3 + 5   = 8
+      Fifth Calculation   5 + 8   = 13
+      Sixth Calculation   8 + 13  = 21
+      Seventh Calculation 13 + 21 = 34
+      ...
+    */
+  }
+
+  public static void main(String[] args) {
+
+    int test_1 = 2;
+    int test_2 = 3;
+    int test_3 = 5;
+    int test_4 = 10;
+
+    System.out.println(climbStairs(test_1));
+    System.out.println(climbStairs(test_2));
+    System.out.println(climbStairs(test_3));
+    System.out.println(climbStairs(test_4));
+ }
+}
